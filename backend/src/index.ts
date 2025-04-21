2dbb5fa7fe2ff78830919ffcd7bc026c95d76f83
Vaasav Sharma <vaasavawasthisharma9@gmail.com>
2025-04-21 20:28:39 +0530

    added hotels with tests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from 'mongoose';
-import userRoutes from './routes/users'; 
-import authRoutes from './routes/auth';
 import cookieParser from 'cookie-parser';
-import path from "path";
+import * as path from "path";
+import { v2 as cloudinary } from "cloudinary";
+
+import userRoutes from './routes/users';
+import authRoutes from './routes/auth';
+import myHotelRoutes from './routes/my-hotels';
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => {
@@ -17,26 +26,28 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
   });
 
 console.log(process.env.FRONTEND_URL);
+
+
 const app = express();
+
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
+
 app.use(cookieParser());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static(path.join("../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api/my-hotels", myHotelRoutes);
 
-app.get("/api/test", async(req: Request,res: Response)=>{
-    res.json({ message: "Hello from express endpoint!" });
-});
 
-app.listen(7000, ()=>{
-    console.log("Server is running on localhost:7000");
+app.listen(7000, () => {
+  console.log("Server is running on http://localhost:7000");
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,8 +7,12 @@ import {
 import Layout from './layouts/Layout';
 import Register from './pages/Register';
 import SignIn from "./pages/SignIn"
+import AddHotel from './pages/AddHotel';
+import { useAppContext } from './contexts/AppContext';
+import MyHotels from './pages/MyHotels';
 
 const App = ()=>{
+  const {isLoggedIn} = useAppContext();
   return (
     <Router>
       <Routes>
@@ -32,6 +36,20 @@ const App = ()=>{
             <SignIn/>
           </Layout>
         }/>
+
+        {isLoggedIn && <>
+          <Route path="/add-hotel" element={
+            <Layout>
+              <AddHotel />
+            </Layout>
+          } />
+
+<Route path="/my-hotels" element={
+            <Layout>
+              <MyHotels />
+            </Layout>
+          } />
+        </>}
         <Route path='*' element={<Navigate to='/'/>}/>
         
       </Routes>
